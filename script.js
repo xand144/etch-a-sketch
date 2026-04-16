8db99316cc921e9db59eaf5ccfe77f1c5e9c4acf
xand144 <174009470+xand144@users.noreply.github.com>
2026-04-16 16:49:10 +0100

    Implement creation of new grid
    * New grid prompt only accepts integer values above 0, with upper limit
      of 100 to avoid performance issues.
    * I have for some reason not been able to update NodeLists automatically
      after new grid is generated.
      As a workaround, "Toggle Grid" button now updates the NodeList when
      clicked. This works because by the time the button is interactable the
      grid has been fully generated.
      For the same reason, the mouse event listener for colouring squares is
      now added during grid generation.

--- script.js
+++ script.js
@@ -5,27 +5,34 @@ function createGrid(squaresPerSide) {
         for (let i = 1; i <= squaresPerSide; i++) {
             const square = document.createElement("div");
             square.classList.add("square", "grid");
+            square.addEventListener("mouseenter", function(e) {
+                e.target.classList.add("black");
+            })
             row.appendChild(square);
         }
         grid.appendChild(row);
+        squares = document.querySelectorAll(".square");
     }
     document.addEventListener("DOMContentLoaded", () => {
-        squares = document.querySelectorAll(".square");
         addEvents();
     })
 }
 
 function replaceGrid() {
-
+    grid.replaceChildren();
+    let size;
+    do {
+        size = prompt("How many squares per side? (Max 100)", 16);
+    } while (isNaN(size) || size === null || size < 1 || size > 100);
+    createGrid(size);
 }
 
 function addEvents() {
-    squares.forEach(square => square.addEventListener("mouseenter", function(e) {
-        e.target.classList.add("black");
-    }))
     btnNewGrid.addEventListener("click", () => {
+        replaceGrid();
     })
     btnGridView.addEventListener("click", () => {
+        squares = document.querySelectorAll(".square");
         squares.forEach(square => square.classList.toggle("grid"));
     })
 }
@@ -34,6 +41,6 @@ const grid = document.querySelector("#grid-container");
 const btnNewGrid = document.querySelector("#new-grid");
 const btnGridView = document.querySelector("#toggle-gridview");
 
-let squares;
+let squares = document.querySelectorAll(".square");
 
 createGrid(16);
